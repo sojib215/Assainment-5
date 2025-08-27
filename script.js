1320e7ecd48f8cd3a3f1949b189b159023a5268e
Sojib215 <sojib.hassan489@gmail.com>
2025-08-27 22:37:25 +0600

    added heroo

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-// Nav Bar
+// Navbar 
 let heartCount = 0;
 let coinCount = 100;
 let copyCount = 0;
-const heartDisplay=document.getElementById("heart");
-const coinDisplay=document.getElementById("coin");
-const copyDisplay=document.getElementById("copy-count");
-const heartDiv=heartDisplay.parentElement;
-heartDiv.addEventListener("click",function(){
-    heartCount++;
-    heartDisplay.textContent=heartCount;
-});
+
+const heartDisplay = document.getElementById("heart");
+const coinDisplay = document.getElementById("coin");
+const copyDisplay = document.getElementById("copy"); 
+
 function reduceCoins(amount){
-    if(coinCount => amount){
+    if(coinCount >= amount){
         coinCount -= amount;
-        coinDisplay.textContent=coinCount;
+        coinDisplay.textContent = coinCount;
         return true;
-    }
-    else{
+    } else {
         alert("Not Enough Coins!!!");
         return false;
     }
 }
+
 function increaseCopyCount(){
     copyCount++;
-    copyDisplay.textContent=coinCount+"copy";
+    copyDisplay.textContent = copyCount + " copy";
 }
+
+const cardHearts = document.querySelectorAll(".card-heart");
+cardHearts.forEach(function(icon){
+    icon.addEventListener("click", function(){
+        heartCount++;
+        heartDisplay.textContent = heartCount;
+    });
+});
